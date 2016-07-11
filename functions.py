300638f7460fed8f980111e1525cb2a9147647a8
ShouxingSun <sunshouxing@gmail.com>
2016-07-11 01:41:57 -0700

    upload already existed notebooks

---
+++ functions.py
@@ -0,0 +1,11 @@
+def func():
+    print "func"
+
+
+def _func():
+    print "_func"
+
+
+def __func():
+    print "__func"
+
---
+++ functions2.py
@@ -0,0 +1,13 @@
+__all__ = ['func', '_func', '__func']
+
+def func():
+    print "func"
+
+
+def _func():
+    print "_func"
+
+
+def __func():
+    print "__func"
+
